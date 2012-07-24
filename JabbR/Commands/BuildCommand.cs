3dd9337cecabca400e05ac55b99cccec539f6e87
Scott Kirkland <srkirkland@gmail.com>
2012-07-24 09:59:15 -0700

    added show command for build configs

--- JabbR/Commands/BuildCommand.cs
+++ JabbR/Commands/BuildCommand.cs
@@ -3,9 +3,16 @@ using System.Linq;
 using System.Net.Http.Headers;
 using JabbR.Models;
 using Newtonsoft.Json.Linq;
+using System;
 
 namespace JabbR.Commands
 {
+    /// <summary>
+    /// Usage: /build [show|run] [buildtype-id]
+    /// /build: for list of projects with their build configurations and ids
+    /// /build show id: to show latest build info for build config with id
+    /// /build run id: to run the build configuration
+    /// </summary>
     [Command("build", "")]
     public class BuildCommand : UserCommand
     {
@@ -31,27 +38,29 @@ namespace JabbR.Commands
             }
             else
             {
-                var buildInfo = new[]
-                                {
-                                    new {Name = "help", Description = "Type /help to show the list of commands"},
-                                    new
-                                        {
-                                            Name = "nick",
-                                            Description =
-                                        "Type /nick [user] [password] to create a user or change your nickname. You can change your password with /nick [user] [oldpassword] [newpassword]"
-                                        },
-                                    new
-                                        {
-                                            Name = "join",
-                                            Description =
-                                        "Type /join [room] [inviteCode] - to join a channel of your choice. If it is private and you have an invite code, enter it after the room name"
-                                        }
-                                };
+                var cmd = args[0]; //TODO: change to support different commands
+                var id = "bt" + args[1];
 
-                context.NotificationService.ShowInfo("Build Information", buildInfo);
+                var client = new System.Net.Http.HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string serviceResult =
+                    client.GetStringAsync(string.Format("http://builder/guestAuth/app/rest/builds/buildType:(id:{0})", id)).Result;
+
+                var jsonResults = JObject.Parse(serviceResult);
+                var buildConfigToken = jsonResults["buildType"];
+                var buildConfigInfo = string.Format("Latest {0} build for {1} <a href='{2}'>{2}</a>",
+                                                    buildConfigToken["name"].Value<string>(),
+                                                    buildConfigToken["projectName"].Value<string>(),
+                                                    buildConfigToken["webUrl"].Value<string>());
+
+                var buildInfo = new[]
+                                    {
+                                        new {Name = "Status", Description = string.Format("[<strong>{0}</strong>] -- {1}", jsonResults["status"].Value<string>(), jsonResults["statusText"].Value<string>())},
+                                        new {Name = "Details", Description = string.Format("<a href='{0}'>{0}</a>", jsonResults["webUrl"].Value<string>())}
+                                    };
+
+                context.NotificationService.ShowInfo(buildConfigInfo, buildInfo);
             }
-                
-            
         }
     }
 }
